f9deddb21539dca681c01e82550a26d699b0e58a
Doug Donahue <ddonahue@redhat.com>
2022-01-20 09:16:48 -0500

    Changed based on PR conversation

--- src/PresentationalComponents/ErrState.js
+++ src/PresentationalComponents/ErrState.js
@@ -2,19 +2,12 @@ import React from 'react';
 import propTypes from 'prop-types';
 import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
 
-export const ErrState = ({
-  errorTitle,
-  errorDescription,
-  hrefUrl,
-  hrefText,
-}) => {
+export const ErrState = ({ errorTitle, errorDescription }) => {
   return (
     <ErrorState
       errorTitle={errorTitle}
       errorDescription={
-        <span>
-          {errorDescription} <br /> <a href={hrefUrl}>{hrefText}</a>
-        </span>
+        <div dangerouslySetInnerHTML={{ __html: errorDescription }} />
       }
     />
   );
@@ -23,6 +16,4 @@ export const ErrState = ({
 ErrState.propTypes = {
   errorTitle: propTypes.string,
   errorDescription: propTypes.string,
-  hrefUrl: propTypes.string,
-  hrefText: propTypes.string,
 };
